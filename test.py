c0239b5e7f5ef85f0f988e67aed6ae4a23e76bd6
zhulp <zhulp@VirCent.(none)>
2015-01-23 15:32:06 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import httplib
+import time
+from time import clock
+
+import logging
+import logging.config
+logging.config.fileConfig("logging.conf")
+#create logger
+logger = logging.getLogger("root")
+
+print "hello"
+logger.info("count:" + "hello" + " status:" + "world")
+exit()
+print "world"
